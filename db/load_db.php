ab1b0060c97a094a931bdd013d0a2f55081ff61e
Adam Quinton <quintonish@gmail.com>
2016-03-05 02:09:38 -0700

    poop

--- db/load_db.php
+++ db/load_db.php
@@ -8,25 +8,14 @@ $dbHost = "";
 $dbUser = "";
 $dbPassword = "";
 $dbName = "journal";
-$openShiftVar = getenv('OPENSHIFT_MYSQL_DB_HOST');
-//if not on openshift
-if ($openShiftVar === null || $openShiftVar == "")
-{
+
   // Not in the openshift environment
   //echo "Using local credentials: ";
   require("set_local_credentials.php");
-}
-else//on openshift
-{
-  // In the openshift environment
-  //echo "Using openshift credentials: ";
-  $dbHost = getenv('OPENSHIFT_MYSQL_DB_HOST');
-  $dbPort = getenv('OPENSHIFT_MYSQL_DB_PORT');
-  $dbUser = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
-  $dbPassword = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
-}
+
 //echo "host:$dbHost:$dbPort dbName:$dbName user:$dbUser password:$dbPassword<br >\n";
 GLOBAL $db;
 $db = new PDO("mysql:host=$dbHost;dbname=$dbName", $dbUser, $dbPassword);
 return $db;
 }
+?>
