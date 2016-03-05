e0925a5a63027281f304199c0f2b5389eebbb646
Adam Quinton <quintonish@gmail.com>
2016-03-04 20:33:25 -0700

    Upload files started in Chrome Developer

---
+++ main.js
@@ -0,0 +1,98 @@
+var element = document.querySelector("#greeting");
+element.innerText = "Scripture Journal";
+
+/***************************
+ * Check Email
+ ***************************/
+function checkEmail(email) {
+  console.log('Email is '+email.value);
+  //TODO: call database to check email
+  var emailConfirm = document.querySelector("#"+email.getAttribute('id')+"-confirm");
+  emailConfirm.className = "label label-success";
+  emailConfirm.innerHTML = 'Email Found';
+  emailConfirm.className = "label label-danger";
+  emailConfirm.innerHTML = 'Invalid email';
+}
+
+/***************************
+ * Upload or pull journal data
+ * depending on json
+ ***************************/
+function database(stringified, url) {
+  var http = new XMLHttpRequest(); 
+  http.open("POST", url, true);
+  http.setRequestHeader("Content-type", "application/json; charset=utf-8");
+  http.onreadystatechange = function() {
+    if (http.readyState == 4 && http.status == 200) {
+    	//response
+    	var data = (http.responseText);
+    	console.log(data);
+    	data = JSON.parse(data);
+    	for (var i = 0; i < data.times.length; i++) {
+			  var foundHour = data.times[i].hour;
+    	}
+    }
+  };
+  http.send(stringified);
+}
+
+/***************************
+ * COUNT WORDS
+ ***************************/
+function countWords(idOfBox, numWordsRequired) {
+  var box = document.querySelector("#"+idOfBox);
+  //console.log('Box content: ' + box.value);
+  var words = 1;
+  //count words
+  for (var i = 0; i < box.value.length; i++) {
+    if (box.value[i] == ' ') {
+      words++;
+    }
+  }
+  
+  var counter = document.querySelector("#"+idOfBox+"-counter");
+  counter.innerHTML = words+"/"+numWordsRequired;
+  if (words >= numWordsRequired) {  
+    counter.style.color = 'green';
+  }
+}
+
+/***************************
+ * COPY
+ ***************************/
+(function() {
+
+  'use strict';
+
+  // click events
+  document.body.addEventListener('click', copy, true);
+
+  // event handler
+  function copy(e) {
+
+    // find target element
+    var
+      t = e.target,
+      c = t.dataset.copytarget,
+      inp = (c ? document.querySelector(c) : null);
+
+    // is element selectable?
+    if (inp && inp.select) {
+
+      // select text
+      inp.select();
+
+      try {
+        // copy text
+        document.execCommand('copy');
+        inp.blur();
+      }
+      catch (err) {
+        alert('please press Ctrl/Cmd+C to copy');
+      }
+
+    }
+
+  }
+
+})();
