e5f9acd67a3510410a5c80f1767381a606e47cc1
Adam Quinton <quintonish@gmail.com>
2016-04-23 13:40:30 -0600

    Now set local credentials is required by default

--- db/load_db.php
+++ db/load_db.php
@@ -4,14 +4,13 @@ ini_set("log_errors", 1);
 ini_set("error_log", "/tmp/php-error.log");
 error_log("Load db fr the last timenenene");
 
-$dbHost = "127.0.0.1";
-//WARNING: do not do this
-$dbUser = "root";
+//DANGER: place creds in required file unless you have mafia protection
+$dbHost = "";
+$dbUser = "";
 $dbPassword = "";
 $dbName = "journal";
 
-  //DANGER: uncomment and place creds in that file unless you have mafia protection
-  //require("set_local_credentials.php");
+require("set_local_credentials.php");
 
 GLOBAL $db;
 $db = new PDO("mysql:host=$dbHost;dbname=$dbName", $dbUser, $dbPassword);
