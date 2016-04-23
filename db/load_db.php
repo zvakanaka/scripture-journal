74b06ddb31d1ebbb8b951fb53246985117432bc4
Adam Quinton <quintonish@gmail.com>
2016-04-23 11:45:41 -0600

    More compatible for quick up and running

--- db/load_db.php
+++ db/load_db.php
@@ -1,17 +1,18 @@
 <?php
 function loadDB() {
-  ini_set("log_errors", 1);
+ini_set("log_errors", 1);
 ini_set("error_log", "/tmp/php-error.log");
-error_log( "Load db fr the last ltiemenenene" );
+error_log("Load db fr the last timenenene");
 
-$dbHost = "";
-$dbUser = "";
+$dbHost = "127.0.0.1";
+//WARNING: do not do this
+$dbUser = "root";
 $dbPassword = "";
 $dbName = "journal";
 
-  // Not in the openshift environment
   //echo "Using local credentials: ";
-  require("set_local_credentials.php");
+  //change to require if needed
+  include("set_local_credentials.php");
 
 //echo "host:$dbHost:$dbPort dbName:$dbName user:$dbUser password:$dbPassword<br >\n";
 GLOBAL $db;
